d7d7a638c4f5beb94680ce085a3bba50104df3b0
Alex Batistta <batistta.alex@outlook.com>
2023-12-07 10:24:53 -0300

    base popup part 1

--- src/project/popups/BasePopup.ts
+++ src/project/popups/BasePopup.ts
@@ -1,18 +1,26 @@
-import type { Graphics } from "pixi.js";
-import type { Container } from "pixi.js";
+import { Sprite } from "pixi.js";
+import { Graphics } from "pixi.js";
+import { Container } from "pixi.js";
 import { Tween } from "tweedle.js";
 import { PixiScene } from "../../engine/scenemanager/scenes/PixiScene";
 import type { SDFBitmapText } from "../../engine/sdftext/SDFBitmapText";
+import type { Button } from "../../engine/ui/button/Button";
 import { ColorDictionary } from "../../engine/utils/Constants";
 import { GraphicsHelper } from "../../engine/utils/GraphicsHelper";
 import { setPivotToCenter } from "../../engine/utils/MathUtils";
+import { ScaleHelper } from "../../engine/utils/ScaleHelper";
 
 export class BasePopup extends PixiScene {
 	protected overlay: Graphics;
 	protected background: Graphics;
 	protected title: SDFBitmapText;
 	protected centerContainer: Container;
-	constructor(alpha: number = 1, color: number = ColorDictionary.white) {
+	protected backTop: Graphics;
+	protected backBottom: Graphics;
+	protected logo: Sprite;
+	protected btnClose: Button;
+	protected backgroundContainer: Container;
+	constructor() {
 		super();
 
 		this.overlay = GraphicsHelper.pixel(ColorDictionary.black, 1);
@@ -22,24 +30,63 @@ export class BasePopup extends PixiScene {
 		setPivotToCenter(this.overlay);
 		this.addChild(this.overlay);
 
-		this.background = GraphicsHelper.pixel(color, alpha);
-		setPivotToCenter(this.background);
-		this.addChild(this.background);
+		this.backgroundContainer = new Container();
+		this.addChild(this.backgroundContainer);
+
+		this.background = GraphicsHelper.pixel(ColorDictionary.white);
+		this.background.pivot.x = 0.5;
+		// setPivotToCenter(this.background);
+		this.background.scale.set(1080, 1920);
+		this.backgroundContainer.addChild(this.background);
+
+		this.backTop = GraphicsHelper.pixel(ColorDictionary.black);
+		this.backTop.pivot.x = 0.5;
+		this.backTop.scale.set(1080, 222);
+		this.backTop.y = 130;
+		this.backgroundContainer.addChild(this.backTop);
+
+		this.backBottom = GraphicsHelper.pixel(ColorDictionary.black);
+		this.backBottom.pivot.set(0.5, 1);
+		this.backBottom.scale.set(1080, 222);
+		this.backBottom.y = 1920;
+		this.backgroundContainer.addChild(this.backBottom);
+
+		setPivotToCenter(this.backgroundContainer);
+
+		this.centerContainer = new Container();
+		this.addChild(this.centerContainer);
+
+		this.logo = Sprite.from("logo");
+		setPivotToCenter(this.logo);
+		this.logo.height = 145;
+		this.logo.scale.x = this.logo.scale.y;
+		this.logo.y = this.backBottom.y - this.backBottom.height / 2;
+		this.centerContainer.addChild(this.logo);
+
+		const frame = new Graphics();
+		frame.lineStyle(1, ColorDictionary.white, 0.001).drawRect(-1080 / 2, 0, 1080, 1920);
+		this.centerContainer.addChild(frame);
+		setPivotToCenter(this.centerContainer);
+		this.centerContainer.visible = false;
 	}
 
 	public override onShow(): void {
-		const scaleBack = this.background.scale.x;
-		this.background.scale.x = 0;
+		const scaleContainer = this.backgroundContainer.scale.x;
+		this.backgroundContainer.scale.x = 0;
 		new Tween(this.overlay)
 			.to({ alpha: 0.8 }, 250)
 			.onComplete(() => {
-				new Tween(this.background.scale).to({ x: scaleBack }, 250).start();
+				new Tween(this.backgroundContainer.scale)
+					.to({ x: scaleContainer }, 250)
+					.onComplete(() => (this.centerContainer.visible = true))
+					.start();
 			})
 			.start();
 	}
 
 	private closePopup(): void {
-		new Tween(this.background.scale)
+		this.centerContainer.visible = false;
+		new Tween(this.backgroundContainer.scale)
 			.to({ x: 0 }, 250)
 			.onComplete(() => {
 				new Tween(this.overlay).to({ alpha: 0 }, 250).onComplete(this.closeHandler.bind(this)).start();
@@ -51,7 +98,10 @@ export class BasePopup extends PixiScene {
 		this.overlay.scale.set(newW, newH);
 		this.overlay.position.set(newW * 0.5, newH * 0.5);
 
-		this.background.scale.set(newW * 0.8, newH * 0.8);
-		this.background.position.set(newW * 0.5, newH * 0.5);
+		ScaleHelper.setScaleRelativeToScreen(this.backgroundContainer, newW, newH, 0.9, 0.9);
+		this.backgroundContainer.position.set(newW * 0.5, newH * 0.5);
+
+		this.centerContainer.scale = this.backgroundContainer.scale;
+		this.centerContainer.position = this.backgroundContainer.position;
 	}
 }
