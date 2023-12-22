c26080c19ee6f572e9815bf08d2af33ab991491b
Alex Batistta <batistta.alex@outlook.com>
2023-12-22 12:20:25 -0300

    copy to clipboard

--- src/project/popups/PaymentPopup.ts
+++ src/project/popups/PaymentPopup.ts
@@ -1,15 +1,17 @@
 import i18next from "i18next";
-import { Text } from "pixi.js";
+import { Container, Sprite, Text } from "pixi.js";
 import { Manager } from "../..";
-import { TextStyleDictionary } from "../../engine/utils/Constants";
+import { Button } from "../../engine/ui/button/Button";
+import { ColorDictionary, TextStyleDictionary } from "../../engine/utils/Constants";
+import { GraphicsHelper } from "../../engine/utils/GraphicsHelper";
 import { setPivotToCenter } from "../../engine/utils/MathUtils";
 import { BasePopup } from "./BasePopup";
 
 export class PaymentPopup extends BasePopup {
 	private subtitle1: Text;
-	private cvuText: Text;
+	private cvuContent: Container;
 	private subtitle2: Text;
-	private aliasText: Text;
+	private aliasContent: Container;
 	constructor() {
 		super(i18next.t("PPPayment.title"));
 
@@ -17,23 +19,101 @@ export class PaymentPopup extends BasePopup {
 		setPivotToCenter(this.subtitle1);
 		this.centerContainer.addChild(this.subtitle1);
 
-		this.cvuText = new Text(i18next.t("PPPayment.cvu"), TextStyleDictionary.textBlack);
-		setPivotToCenter(this.cvuText);
-		this.centerContainer.addChild(this.cvuText);
+		this.cvuContent = new Container();
+		this.centerContainer.addChild(this.cvuContent);
+
+		const cvuBack = GraphicsHelper.pixel(ColorDictionary.black);
+		setPivotToCenter(cvuBack);
+		cvuBack.scale.set(760, 70);
+		cvuBack.x = -50;
+		this.cvuContent.addChild(cvuBack);
+
+		const cvuText = new Text(i18next.t("PPPayment.cvu"), TextStyleDictionary.textWhite);
+		setPivotToCenter(cvuText);
+		cvuText.x = -50;
+		this.cvuContent.addChild(cvuText);
+
+		const btnCvuContent: Container = new Container();
+		const btnCvuBack = GraphicsHelper.pixel(ColorDictionary.black);
+		setPivotToCenter(btnCvuBack);
+		btnCvuBack.scale.set(87, 70);
+		btnCvuContent.addChild(btnCvuBack);
+		const btnCvuSprite = Sprite.from("package-1/copy.png");
+		setPivotToCenter(btnCvuSprite);
+		btnCvuSprite.scale.set(40 / btnCvuSprite.texture.width);
+		btnCvuContent.addChild(btnCvuSprite);
+
+		const btnCvu = new Button({
+			defaultState: { content: btnCvuContent },
+			onClick: () => {
+				const textarea = document.createElement("textarea");
+				textarea.value = cvuText.text;
+				document.body.appendChild(textarea);
+				textarea.select();
+				document.execCommand("copy");
+				document.body.removeChild(textarea);
+			},
+			highlightState: { scale: 1.05, tween: true },
+			fixedCursor: "pointer",
+		});
+		btnCvu.x = 386;
+		this.cvuContent.addChild(btnCvu);
 
 		this.subtitle2 = new Text(i18next.t("PPPayment.subtitle2"), TextStyleDictionary.textBlackBig);
 		setPivotToCenter(this.subtitle2);
 		this.centerContainer.addChild(this.subtitle2);
 
-		this.aliasText = new Text(i18next.t("PPPayment.alias"), TextStyleDictionary.textBlack);
-		setPivotToCenter(this.aliasText);
-		this.centerContainer.addChild(this.aliasText);
+		this.aliasContent = new Container();
+		this.centerContainer.addChild(this.aliasContent);
 
+		const aliasBack = GraphicsHelper.pixel(ColorDictionary.black);
+		setPivotToCenter(aliasBack);
+		aliasBack.scale.set(760, 70);
+		aliasBack.x = -50;
+		this.aliasContent.addChild(aliasBack);
+
+		const aliasText = new Text(i18next.t("PPPayment.alias"), TextStyleDictionary.textWhite);
+		setPivotToCenter(aliasText);
+		aliasText.x = -50;
+		this.aliasContent.addChild(aliasText);
+
+		const btnAliasContent: Container = new Container();
+		const btnAliasBack = GraphicsHelper.pixel(ColorDictionary.black);
+		setPivotToCenter(btnAliasBack);
+		btnAliasBack.scale.set(87, 70);
+		btnAliasContent.addChild(btnAliasBack);
+		const btnAliasSprite = Sprite.from("package-1/copy.png");
+		setPivotToCenter(btnAliasSprite);
+		btnAliasSprite.scale.set(40 / btnAliasSprite.texture.width);
+		btnAliasContent.addChild(btnAliasSprite);
+
+		const btnAlias = new Button({
+			defaultState: { content: btnAliasContent },
+			onClick: () => {
+				const textarea = document.createElement("textarea");
+				textarea.value = aliasText.text;
+				document.body.appendChild(textarea);
+				textarea.select();
+				document.execCommand("copy");
+				document.body.removeChild(textarea);
+			},
+			highlightState: { scale: 1.05, tween: true },
+			fixedCursor: "pointer",
+		});
+		btnAlias.x = 386;
+		this.aliasContent.addChild(btnAlias);
+
+		setPivotToCenter(this.cvuContent);
+		setPivotToCenter(this.aliasContent);
 		this.onChangeOrientation();
 	}
 
 	public override onChangeOrientation(): void {
 		if (Manager.isPortrait) {
+			this.subtitle1.y = 700;
+			this.cvuContent.y = 815;
+			this.subtitle2.y = 1100;
+			this.aliasContent.y = 1215;
 		} else {
 		}
 	}
